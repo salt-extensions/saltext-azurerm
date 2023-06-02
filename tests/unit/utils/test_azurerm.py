d851ac82abe2e3dc31baa1563255c982ddc475e0
Natalie Lee <natalieleecodes@gmail.com>
2023-06-02 16:07:21 +0000

    wip test compare lists of dicts

--- tests/unit/utils/test_azurerm.py
+++ tests/unit/utils/test_azurerm.py
@@ -88,7 +88,6 @@ def test_log_cloud_error():
     ],
 )
 def test_get_client(client_type, client_object, mock_determine_auth):
-
     if client_type == "NOT_THERE":
         with pytest.raises(SaltSystemExit):
             saltext.azurerm.utils.azurerm.get_client(client_type)
@@ -130,3 +129,55 @@ def test_create_object_model():
     )
     assert "VirtualNetwork" in str(obj.__class__)
     assert obj.as_dict() == {"enable_ddos_protection": False, "enable_vm_protection": False}
+
+
+def test_compare_list_of_dicts():
+    # equal
+    old1 = [
+        {"name": "group1", "location": "eastus", "type": "ResourceGroup"},
+        {"name": "group2", "location": "eastus"},
+    ]
+    new1 = [
+        {"name": "group1", "location": "eastus"},
+        {"name": "group2", "location": "eastus"},
+    ]
+
+    # not equal
+    old2 = [
+        {"name": "group1", "location": "eastus"},
+        {"name": "group2", "location": "eastus"},
+    ]
+    new2 = [
+        {"name": "group1", "location": "westus"},
+        {"name": "group2", "location": "eastus"},
+    ]
+
+    # missing name key
+    old3 = [
+        {"name": "group1", "location": "eastus"},
+        {"name": "group2", "location": "eastus"},
+    ]
+    new3 = [
+        {"name": "group1", "location": "eastus"},
+        {"location": "eastus"},
+    ]
+
+    ret = saltext.azurerm.utils.azurerm.compare_list_of_dicts(old1, new1)
+    assert not ret
+
+    ret = saltext.azurerm.utils.azurerm.compare_list_of_dicts(old2, new2)
+    expected = {
+        "changes": {
+            "old": old2,
+            "new": new2,
+        }
+    }
+    assert ret == expected
+
+    ret = saltext.azurerm.utils.azurerm.compare_list_of_dicts(old3, new3)
+    expected = {"comment": 'configuration dictionaries must contain the "name" key!'}
+    assert ret == expected
+
+
+def test_determine_auth():
+    assert False
