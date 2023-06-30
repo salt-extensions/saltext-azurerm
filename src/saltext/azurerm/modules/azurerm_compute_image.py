3e6316cb29d9e686b08b25cbe11fe59f66856c0e
Natalie Lee <natalieleecodes@gmail.com>
2023-06-30 20:38:10 +0000

    unexpected indent issue

--- src/saltext/azurerm/modules/azurerm_compute_image.py
+++ src/saltext/azurerm/modules/azurerm_compute_image.py
@@ -23,14 +23,16 @@ Azure Resource Manager (ARM) Compute Image Execution Module
     if using managed identity:
       * ``subscription_id``
 
-    Optional provider parameters:
+Optional provider parameters:
 
     **cloud_environment**: Used to point the cloud driver to different API endpoints, such as Azure GovCloud.
+
     Possible values:
-      * ``AZURE_PUBLIC_CLOUD`` (default)
-      * ``AZURE_CHINA_CLOUD``
-      * ``AZURE_US_GOV_CLOUD``
-      * ``AZURE_GERMAN_CLOUD``
+
+        * ``AZURE_PUBLIC_CLOUD`` (default)
+        * ``AZURE_CHINA_CLOUD``
+        * ``AZURE_US_GOV_CLOUD``
+        * ``AZURE_GERMAN_CLOUD``
 
 """
 # Python libs
